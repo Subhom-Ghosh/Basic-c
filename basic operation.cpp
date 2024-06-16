42289a8c67ff2b4edd8e61524636e85fa18265c4
Subhom-Ghosh <subhomghosh836@gmail.com>
2024-06-16 08:40:54 +0530

    Add files via upload

---
+++ basic operation.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float a,b;
+	printf("Enter two numbers ");
+	scanf("%f%f",&a,&b);
+	printf("Addition = %f",a+b);
+		printf("\nSubstraction = %f",a-b);
+			printf("\nMultiplication = %f",a*b);
+				printf("\nDivision = %f",a/b);
+			    	return 0;	
+}
