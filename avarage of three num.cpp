b51eb0490138ba4ae3cd4f7de3f6f34db934eb17
Subhom-Ghosh <subhomghosh836@gmail.com>
2024-06-16 08:46:58 +0530

    Add files via upload

---
+++ avarage of three num.cpp
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	float a,b,c,avg;
+	printf("Enter three numbers for avarage it ");
+	scanf("%f%f%f",&a,&b,&c);
+	avg=(a+b+c)/3;
+	printf("avarage= %f",avg);
+	return 0;
+}
+		
